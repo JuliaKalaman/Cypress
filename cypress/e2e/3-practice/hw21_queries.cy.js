365058a17cc9a3ba1c3ea112bb3f9303e5b97c43
JuliaKalaman <kalaman.julia@gmail.com>
2024-08-02 10:15:58 +0200

    adding semicolon

--- cypress/e2e/3-practice/hw21_queries.cy.js
+++ cypress/e2e/3-practice/hw21_queries.cy.js
@@ -17,34 +17,34 @@ describe('Swag Labs Login Form', () => {
         cy.get('[data-test="username"]').type('false_user');
         cy.get('[data-test="password"]').type('secret_sauce');
         cy.get('[data-test="login-button"]').click();
-        cy.get('[data-test="error"]').should('have.text', 'Epic sadface: Username and password do not match any user in this service')
+        cy.get('[data-test="error"]').should('have.text', 'Epic sadface: Username and password do not match any user in this service');
     })
 
     it('Wrong password. Negative test 2', () => {
         cy.get('[data-test="username"]').type('standard_user');
         cy.get('[data-test="password"]').type('false_password');
         cy.get('[data-test="login-button"]').click();
-        cy.get('[data-test="error"]').should('have.text', 'Epic sadface: Username and password do not match any user in this service')
+        cy.get('[data-test="error"]').should('have.text', 'Epic sadface: Username and password do not match any user in this service');
     })
 
     it('Empty username field. Negative test 3', () => {
         cy.get('[data-test="password"]').type('secret_sauce');
         cy.get('[data-test="login-button"]').click();
-        cy.get('[data-test="error"]').should('have.text', 'Epic sadface: Username is required')
+        cy.get('[data-test="error"]').should('have.text', 'Epic sadface: Username is required');
     })
 
     it('Empty password field. Negative test 4', () => {
         cy.get('[data-test="username"]').type('standard_user');
         cy.get('[data-test="login-button"]').click();
-        cy.get('[data-test="error"]').should('have.text', 'Epic sadface: Password is required')
+        cy.get('[data-test="error"]').should('have.text', 'Epic sadface: Password is required');
     })
 
     it('From negative to Positive login test', () => {
         cy.get('[data-test="login-button"]').click();
-        cy.get('[data-test="error"]').should('have.text', 'Epic sadface: Username is required')
+        cy.get('[data-test="error"]').should('have.text', 'Epic sadface: Username is required');
         cy.get('[data-test="username"]').type('standard_user');
         cy.get('[data-test="login-button"]').click();
-        cy.get('[data-test="error"]').should('have.text', 'Epic sadface: Password is required')
+        cy.get('[data-test="error"]').should('have.text', 'Epic sadface: Password is required');
         cy.get('[data-test="password"]').type('secret_sauce');
         cy.get('[data-test="login-button"]').click();
         cy.get('[data-test="title"]').should('have.text', 'Products');
